3c99c773eab77313349266f2aea258500039f78b
Nicholas Miceli <nmelementg@gmail.com>
2019-07-18 20:09:16 -0400

    Completed the challenge prompts including making a new level, adding enemies and pickups, text elements to track and result these things, and music and animation.

---
+++ Maze/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public float min=2f;
+    public float max=3f;
+    public float speed = 1.0f;
+    void Start()
+    {
+        min=transform.position.y;
+        max=transform.position.y+3;
+    }
+
+    void Update()
+    {
+         transform.position =new Vector3 (transform.position.x, Mathf.PingPong(Time.time*2,max-min)+min, transform.position.z);
+    }
+}
--- Maze/Assets/Scripts/PlayerController.cs
+++ Maze/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+    public Text countText;
+    public Text winText;
+    public Text lifeText;
+    public Text loseText;
     public float speed;
     public float jumpForce;
+    public AudioSource tick;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        loseText.text = "";
+        SetCountText();
+        SetLivesText();
+        tick = GetComponent<AudioSource> ();
     }
     void Update()
     {
@@ -24,7 +39,7 @@ public class PlayerController : MonoBehaviour
             Application.Quit();}
     }
 
-    void OnCollisonStay2D(Collision2D collision)
+     void OnCollisionStay2D(Collision2D collision)
     {
         if(collision.collider.tag == "Ground")
         {
@@ -32,6 +47,49 @@ public class PlayerController : MonoBehaviour
             {
                 rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); 
             }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText();
+            NextLevel();
+        }
+        if (other.gameObject.CompareTag("Enemy Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            lives = lives - 1;
+            SetCountText();
+            SetLivesText();
+        }
+
+    }
+    void SetCountText()    
+    {
+        countText.text = "Score: " + count.ToString ();
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
+            tick.Play();
+        }
+    }
+    void SetLivesText()
+    {
+        lifeText.text = "Lives: " + lives.ToString ();
+        if (lives <= 0)
+        {
+            loseText.text = "You Lose...";
+            gameObject.SetActive(false);
+        }
+    }
+    void NextLevel()
+    {
+        if (count == 4)
+        {
+         transform.position = new Vector3 (23.68f,0,0);
         }
     }
 }
